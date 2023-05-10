46cd0b2fa24d52f88939f0cbdfefe35d41dccafb
eminishimoto <eminishimoto5@gmail.com>
2023-05-10 12:56:15 +0000

    chaged home with adding routing.modulets and module.ts

--- ionicapp/src/app/home/home.page.ts
+++ ionicapp/src/app/home/home.page.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonicModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  login_password:any;
+  login_email:any;
+  constructor(public router: Router) {}
+  loginForm(e:any){
+    console.log("requested data:",e.value);
+    console.log("this is not a safe way to pass data!!!!");
+    let url="/home2";
+    let url_withparameter="/home2?email="
+        +e.value.login_email+"&password="
+        +e.value.login_password;
+    this.router.navigateByUrl(url_withparameter);
+  }
+
+  ngOnInit(){}
+
 }
