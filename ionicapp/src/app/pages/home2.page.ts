e25d2b0381ea173e39b9af5c0f3f40b5ee790c8d
eminishimoto <eminishimoto5@gmail.com>
2023-05-10 13:16:34 +0000

    created pages/home2

---
+++ ionicapp/src/app/pages/home2.page.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-home2',
+  templateUrl: './home2.page.html',
+  styleUrls: ['./home2.page.scss'],
+})
+export class Home2Page  {
+
+  req_email:any;
+  req_param:any;
+  constructor(public router:Router) { 
+  
+  console.log("this is not a safe way to pass data!!!! Just an example");
+  console.log(this.router.getCurrentNavigation());
+  
+  //this.req_param = this.router.getCurrentNavigation()?.finalUrl;
+  this.req_param = this.router.getCurrentNavigation()?.finalUrl?.queryParams;
+  this.req_email = this.req_param.email;
+  }
+}
